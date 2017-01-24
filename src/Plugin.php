baff77dcaf0b8ead5f51ccb3fcc60b003818c079
Alexey Zauzin <azauzin@scnsoft.com>
2017-01-24 20:04:40 +0300

    Plugin && PluginContainer documentation

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,11 +7,36 @@
 
 namespace Pluginator;
 
-
+/**
+ * Class Plugin
+ *
+ * Plugin entity. This class contains all the methods
+ * needed to work with one concrete plugin.
+ *
+ * Example of usage:
+ * if ($plugin->validate() && $plugin->isActive()) {
+ *      $plugin->load();
+ *      $plugin->init();
+ * }
+ *
+ * @package Pluginator
+ */
 class Plugin
 {
+    /**
+     * Entry point script. Should be provided after
+     * the plugin loading.
+     * @var string
+     */
     private $entryPoint;
 
+    /**
+     * Performs a validation checks for a structure of the plugin.
+     * Remember that it doesn't includes any code checks!
+     *
+     * @param string $dir
+     * @return bool
+     */
     public function validate($dir) {
         if (!file_exists("$dir/plugin.php")) {
             return false;
@@ -19,21 +44,48 @@ class Plugin
         return true;
     }
 
+    /**
+     * Loads a plugin, so it's entry point will be bound.
+     * Remember that it's better for you to consider to
+     * check it first!
+     *
+     * Binding an event handlers is another method called init();
+     *
+     * E.g.
+     * if ($plugin->validate()) {
+     *      $plugin->load();
+     *      $plugin->init();
+     * }
+     *
+     * @param $dir
+     */
     public function load($dir) {
         $this->entryPoint = $dir.'/plugin.php';
     }
 
+    /**
+     * @todo Checks if this plugin is enabled.
+     * This functionality is not implemented yet.
+     *
+     * @return bool
+     */
     public function isActive() {
         return true;
     }
 
+    /**
+     * Bounds all the events of this plugin to EventHandler.
+     * Remember that you have to load() it first.
+     *
+     * @throws PluginatorException
+     */
     public function init() {
         if ($this->entryPoint === null) {
             throw new PluginatorException("Plugin must be loaded before use!");
         }
 
         if (!is_file($this->entryPoint)) {
-            throw new PluginatorException("Plugin is damaged.");
+            throw new PluginatorException("Plugin entry point file is damaged.");
         }
 
         include_once $this->entryPoint;
--- src/PluginContainer.php
+++ src/PluginContainer.php
@@ -9,11 +9,40 @@ namespace Pluginator;
 
 use Traversable;
 
+/**
+ * Class PluginContainer
+ *
+ * This is Traversable entity which is a container of all
+ * the plugins installed. Please notice that it's not a container
+ * of active plugins only. Every plugin installed is being loaded
+ * in this list, no matter if it's active or not.
+ *
+ * Could be iterated for looping through all the plugins:
+ * foreach ($pluginContainer as $plugin) {
+ *      $plugin->load();
+ * }
+ *
+ * @package Pluginator
+ */
 class PluginContainer implements \IteratorAggregate
 {
+    /**
+     * List of all plugins
+     * @var Plugin[]
+     */
     private $plugins = [];
+
+    /**
+     * Plugins directory
+     * @var string
+     */
     private $pluginsDir;
 
+    /**
+     * PluginContainer constructor.
+     * @param string $pluginsDir
+     * @throws PluginatorException
+     */
     public function __construct(string $pluginsDir = '/plugins/')
     {
         if (!file_exists($pluginsDir) || !is_dir($pluginsDir)) {
@@ -23,6 +52,12 @@ class PluginContainer implements \IteratorAggregate
         $this->pluginsDir = $pluginsDir;
     }
 
+    /**
+     * Scans the plugins directory, and loads them.
+     * This function does nothing with event handlers,
+     * just returns the list of plugins intalled.
+     * @return Plugins[]
+     */
     private function scanPluginDir() {
         $dirs = $this->getDirectories($this->pluginsDir);
 
@@ -38,10 +73,18 @@ class PluginContainer implements \IteratorAggregate
         return $plugins;
     }
 
+    /**
+     * Gets the list of subdirectories of plugins dir.
+     * @param $dir
+     * @return array
+     */
     private function getDirectories($dir) {
         return glob($dir.'/*', GLOB_ONLYDIR);
     }
 
+    /**
+     * Loads all the plugins installed.
+     */
     public function loadPlugins() {
         EventHandler::clearEventMap();
         $plugins = $this->scanPluginDir();
@@ -55,12 +98,16 @@ class PluginContainer implements \IteratorAggregate
         $this->plugins = $plugins;
     }
 
+    /**
+     * Returns all the plugins loaded.
+     * @return Plugin[]
+     */
     public function getPlugins() {
         return $this->plugins;
     }
 
     /**
-     * Retrieve an external iterator
+     * Retrieve an external iterator of all the plugins loaded.
      * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
      * @return Traversable An instance of an object implementing <b>Iterator</b> or
      * <b>Traversable</b>
