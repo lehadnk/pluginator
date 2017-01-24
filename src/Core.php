68f58cb0485393b9405d2c95390df83dee520ffd
Alexey Zauzin <azauzin@scnsoft.com>
2017-01-24 19:32:58 +0300

    Core documented. Fixed the small issue which could cause default variables to return when variable is stated in the config.

--- src/Core.php
+++ src/Core.php
@@ -8,9 +8,19 @@
 
 namespace Pluginator;
 
+/**
+ * Core class of the pluginator package.
+ * Contains all the system operations.
+ *
+ * If you're looking for event binders and triggers,
+ * you should take a look on EventHandler instead.
+ * @see EventHandler
+ * @package Pluginator
+ */
 class Core
 {
     /**
+     * Storage of all plugins loaded.
      * @var PluginContainer;
      */
     private static $pluginContainer;
@@ -24,27 +34,54 @@ class Core
         'pluginsDir' => '/plugins/',
     ];
 
+    /**
+     * Plugin system initialization method which prepares the system
+     * to usage. This method (adding to setConfig which isn't mandatory
+     * at this point) should be the only method user have to call
+     * before using the plugins.
+     */
     static function init() {
         self::$pluginContainer = new PluginContainer(self::getConfig('pluginsDir'));
         self::$pluginContainer->loadPlugins();
     }
 
+    /**
+     * Sets the config array of the pluginator.
+     * Should be assoc array.
+     *
+     * Could be changed run-time, but remember
+     * that you have to reload the plugins after!
+     * @param array $config
+     */
     static public function setConfig(array $config) {
         self::$config = $config;
     }
 
+    /**
+     * Gets the setting from the config loaded. Default value
+     * could be provided and will be returned if no such section
+     * found.
+     * Use with no params for getting assoc array containing
+     * all the settings.
+     * @param string $var
+     * @param any $default
+     * @return any
+     */
     static public function getConfig($var = null, $default = null) {
         if ($var === null) {
             return self::$config;
         }
 
-        if (!isset(self::$config[$var])) {
+        if (!array_key_exists($var, self::$config)) {
             return $default;
         }
 
         return self::$config[$var];
     }
 
+    /**
+     * Allows to reload plugins during the run-time.
+     */
     static public function reloadPlugins() {
         self::$pluginContainer->loadPlugins();
     }
